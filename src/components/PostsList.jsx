9ca365905bcb0aa7550f89b3640ce1538609111f
Genadi Kolev <genadimkolev@gmail.com>
2023-07-27 11:41:58 +0300

    feat: Show posts from mock-api

---
+++ src/components/PostsList.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+function PostsList() {
+    const posts = useSelector(state => state.posts)
+
+    const renderedPosts = posts.map(post => (
+        <article className="post-excerpt" key={post.id}>
+          <h3>{post.title}</h3>
+          <p className="post-content">{post.content.substring(0, 100)}</p>
+        </article>
+      ))
+
+    return ( 
+        <div>
+            <section className='posts-list'>
+                <h2>Posts</h2>
+                {renderedPosts}
+            </section>
+        </div>
+     );
+}
+
+export default PostsList;
